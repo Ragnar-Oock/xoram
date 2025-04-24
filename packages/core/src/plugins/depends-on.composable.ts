0efb06196e3708cdfd151c74fe59faa77f537022
Ragnar-Oock <u.drgnk@outlook.com>
2025-04-24 13:26:39 +0200

    feat(core)!: check for plugin phase in dependsOn
    - remove active plugin injection, as using dependsOn outside a plugin setup doesn't make sense

--- packages/core/src/plugins/depends-on.composable.ts
+++ packages/core/src/plugins/depends-on.composable.ts
@@ -7,26 +7,23 @@ import {getActivePlugin, type PluginId} from "./define-plugin";
  *
  * @public
  */
-export function dependsOn(dependency: PluginId): void;
-/**
- * Mark the plugin as depending on the given dependency.
- * @param dependency the other plugin you depend on, can be an id or a fully resolved plugin. If you
- * are using an id make sure to have at least one import in your module pointing to the dependency
- * so typescript knows to load its types.
- * @param _activePlugin the current plugin, automatically injected if you are in a plugin setup
- * function, you will need to provide it yourself in other context, this is an escape hatch used for
- * testing or build time extension, you should not need to use it.
- *
- * @internal use the public override
- */
-export function dependsOn(dependency: PluginId, _activePlugin = getActivePlugin()): void {
-  if (!_activePlugin) {
+export function dependsOn(dependency: PluginId): void {
+  const activePlugin = getActivePlugin()
+
+  if (!activePlugin) {
     if (import.meta.env.DEV) {
-      console.warn(new Error("Invoked dependsOn with no activePlugin"));
+      console.warn(new Error("Invoked dependsOn with no activePlugin, dependsOn cannot be used outside of a plugin setup function"));
     }
     return;
   }
 
-  _activePlugin.dependencies.push(dependency);
+  if(activePlugin.phase !== 'setup') {
+    if (import.meta.env.DEV) {
+      console.warn(new Error("Invoked dependsOn outside of the setup phase, dependsOn cannot be used in hooks or outside of a plugin setup function"));
+    }
+    return;
+  }
+
+  activePlugin.dependencies.push(dependency);
   return;
 }
