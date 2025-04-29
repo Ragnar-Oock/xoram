6479beccd61df548dfcc88308f545c46e553001c
Ragnar-Oock <u.drgnk@outlook.com>
2025-04-29 19:01:10 +0200

    feat(core): remove necessary bind in emitter wrapper

--- packages/core/src/emitter.ts
+++ packages/core/src/emitter.ts
@@ -4,7 +4,7 @@ import { handleError } from './error-handling';
 
 export function emitter<Events extends Record<EventType, unknown>>(): Emitter<Events> {
 	const instance = mitt<Events>();
-	const on = instance.on.bind(instance);
+	const on = instance.on;
 
 	instance.on = ((type: string | symbol, handler: (...args: unknown[]) => void): void => {
 		on(type, (...args: unknown[]): void => {
