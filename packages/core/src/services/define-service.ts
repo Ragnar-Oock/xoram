00bd419866be33e56ec9cca6726d29a911a2d192
Ragnar-Oock <u.drgnk@outlook.com>
2025-04-30 23:32:14 +0200

    feat(core): pass the emitter instance to the service factory of defineService

--- packages/core/src/services/define-service.ts
+++ packages/core/src/services/define-service.ts
@@ -1,12 +1,20 @@
+import type { Emitter } from 'mitt';
 import type { Application } from '../application';
-import { emitter } from '../emitter';
+import { emitter as createEmitter } from '../emitter';
 import type { Service, ServiceNotifications } from './services.type';
 
-export function defineService<notification extends Record<string, unknown> = Record<string, never>, service extends Record<string, unknown> = Record<string, unknown>>(setup: (app: Application) => service): (app: Application) => Service<notification & ServiceNotifications> & service {
+export function defineService<notification extends Record<string, unknown> = Record<string, never>>(): () => Service<notification & ServiceNotifications>;
+export function defineService<
+	notification extends Record<string, unknown> = Record<string, never>,
+	service extends Record<string, unknown> = Record<string, unknown>
+>(
+	setup?: (app: Application, emitter: Emitter<notification & ServiceNotifications>) => service
+): (app: Application) => Service<notification & ServiceNotifications> & service {
 	return (app): Service<notification & ServiceNotifications> & service => {
+		const emitter = createEmitter<notification & ServiceNotifications>();
 		return {
-			...setup(app),
-			emitter: emitter<notification & ServiceNotifications>(),
+			...(setup ? setup(app, emitter) : {} as service),
+			emitter,
 		}
 	}
 }
