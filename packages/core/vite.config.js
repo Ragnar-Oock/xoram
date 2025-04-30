44cc6573594cef571aa9ff8e38b3d0f03d0b44c3
Ragnar-Oock <u.drgnk@outlook.com>
2025-04-30 16:53:25 +0200

    chore(core): add build script
    - add development build
    - add production build

--- packages/core/vite.config.js
+++
@@ -1,3 +0,0 @@
-import ViteConfig from '@repo/config-vite/vite.config.js';
-
-export default ViteConfig;
---
+++ packages/core/vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig(({mode}) => ({
+    build: {
+        lib: {
+            entry: './src/index.ts',
+            formats: ['es'],
+            fileName: `core.${mode}`
+        },
+        rollupOptions: {
+            external: ['mitt'],
+        },
+        minify: mode === 'development' ? false : 'esbuild',
+        target: 'es2020',
+	      emptyOutDir: false,
+    },
+}))
