ce787ae542689f03af41d99a5fdacf29eeb2d976
Ragnar-Oock <u.drgnk@outlook.com>
2025-05-09 20:08:20 +0200

    feat(core-size-check): first draft of asyncPlugin

---
+++ packages/core/src/plugins/async-plugin.ts
@@ -0,0 +1,57 @@
+import type { Application } from '../application';
+import { handleError } from '../error-handling';
+import { getActivePlugin } from './active-plugin';
+import { addPlugins } from './add-plugin';
+import { toArray } from './array.helper';
+import type { PluginDefinition } from './define-plugin';
+import { definePlugin } from './define-plugin';
+import { onCreated } from './define-plugin-hooks';
+import { dependsOn } from './depends-on.composable';
+import type { DefinedPlugin, PluginId } from './plugin.type';
+import { removePlugin } from './remove-plugin';
+
+export type AsyncPluginErrors = 'asyncPluginCondition' | 'asyncPluginImport'
+
+/**
+ * Load a set of plugins in the application when a condition is met
+ *
+ * @param importer a function to load the plugins asynchronously
+ * @param when the condition to await
+ * @param dependencies a list of plugins that the condition depends on
+ * @param done called once the plugins are added to the app, only used during testing
+ *
+ * @returns a plugin to set up the condition and load the plugin when it is met
+ */
+export function asyncPlugin(
+	importer: () => Promise<PluginDefinition | PluginDefinition[]>,
+	when: (app: Application) => Promise<void>,
+	dependencies?: PluginId[],
+	done?: (app: Application, plugin: DefinedPlugin) => void,
+): PluginDefinition {
+	return definePlugin(() => {
+		dependencies?.forEach(dependsOn);
+
+		onCreated(app => {
+			const plugin = getActivePlugin() as DefinedPlugin;
+			const finished = when(app)
+				.then(
+					importer,
+					error => {
+						handleError(error as Error | string, plugin, app, 'asyncPluginCondition');
+						return [] as PluginDefinition[];
+					},
+				)
+				.then(
+					plugins => addPlugins(toArray(plugins), app),
+					error => handleError(error as Error | string, plugin, app, 'asyncPluginImport'),
+				).then(() => {
+					removePlugin(plugin.id, app);
+				});
+			if (import.meta.env.TEST) {
+				finished.finally(() => {
+					done?.(app, plugin);
+				});
+			}
+		});
+	});
+}
---
+++ packages/core/tests/specs/plugins/async-plugin.spec.ts
@@ -0,0 +1,389 @@
+import { beforeEach, describe, expect, it, type Mock, type MockInstance, vi } from 'vitest';
+import { asyncPlugin, createApp, definePlugin, dependsOn, destroyApp, removePlugin } from '../../../src';
+import { pluginSymbol } from '../../../src/application/application.type';
+import { getActivePlugin } from '../../../src/plugins/active-plugin';
+import { warnParams } from '../../../src/warn.helper';
+import { noop } from '../../dummies/noop';
+
+const purePlugin = definePlugin('A', noop);
+const dependentPlugin = definePlugin('B', () => {
+	dependsOn(purePlugin.id);
+});
+const circularDep1 = definePlugin('C1', () => {
+	dependsOn(circularDep2.id);
+});
+const circularDep2 = definePlugin('C2', () => {
+	dependsOn(circularDep1.id);
+});
+
+describe('asyncPlugin', () => {
+	let done: PromiseWithResolvers<void>,
+		when: PromiseWithResolvers<void>,
+		spy: Mock,
+		warnSpy: MockInstance<(...data: any[]) => void>;
+	beforeEach(() => {
+		done = Promise.withResolvers<void>();
+		when = Promise.withResolvers<void>();
+		spy = vi.fn();
+		warnSpy = vi.spyOn(console, 'warn');
+	});
+	describe('it capture errors thrown by the callbacks and abort', () => {
+		it('should capture errors thrown synchronously by "when"', () => {
+			const error = new Error('catch me');
+			const app = createApp([
+				asyncPlugin(
+					async () => purePlugin,
+					() => {
+						throw error;
+					},
+					[],
+				),
+			], {
+				onError: spy,
+			});
+
+			expect(spy).toHaveBeenCalledExactlyOnceWith(error);
+			expect(app[pluginSymbol].has(purePlugin.id), 'plugins should not be loaded if an error occurred').toBeFalsy();
+		});
+		it('should capture errors thrown asynchronously by "when"', async () => {
+			const error = new Error('catch me');
+			const app = createApp([
+				asyncPlugin(
+					async () => purePlugin,
+					async () => {
+						throw error;
+					},
+					[],
+					() => done.resolve(),
+				),
+			], {
+				onError: spy,
+			});
+
+			await done.promise;
+
+			expect(spy).toHaveBeenCalledExactlyOnceWith(error);
+			expect(app[pluginSymbol].has(purePlugin.id), 'plugins should not be loaded if an error occurred').toBeFalsy();
+		});
+		it('should capture errors thrown synchronously by "importer"', async () => {
+			const error = new Error('catch me');
+			createApp([
+				asyncPlugin(
+					() => {
+						throw error;
+					},
+					async () => void 0,
+					[],
+					() => done.resolve(),
+				),
+			], {
+				onError: spy,
+			});
+
+			await done.promise;
+
+			expect(spy).toHaveBeenCalledExactlyOnceWith(error);
+		});
+		it('should capture errors thrown asynchronously by "importer"', async () => {
+			const error = new Error('catch me');
+			createApp([
+				asyncPlugin(
+					async () => {
+						throw error;
+					},
+					async () => void 0,
+					[],
+					() => done.resolve(),
+				),
+			], {
+				onError: spy,
+			});
+
+			await done.promise;
+
+			expect(spy).toHaveBeenCalledExactlyOnceWith(error);
+		});
+	});
+	describe('it awaits the condition', () => {
+		it('should invoke the importer after the "when" resolves', async () => {
+			const importer = vi.fn(async () => purePlugin);
+
+			createApp([asyncPlugin(importer, () => when.promise)]);
+
+			expect(importer).toHaveBeenCalledTimes(0);
+
+			when.resolve();
+			await when.promise;
+			expect(importer).toHaveBeenCalledOnce();
+		});
+		it('should provide the app instance to "when"', () => {
+			const when = vi.fn(async _ => void 0);
+
+			const app = createApp([asyncPlugin(async () => purePlugin, when)]);
+
+			expect(when).toHaveBeenCalledExactlyOnceWith(app);
+		});
+		it('should invoke "when" during the "active" phase of the returned plugin', () => {
+			let pluginPhase;
+
+			createApp([asyncPlugin(async () => purePlugin, async () => {
+				pluginPhase = getActivePlugin()?.phase;
+			})]);
+
+			expect(pluginPhase).toBe('active');
+		});
+	});
+	describe('it expect dependencies', () => {
+		it('should prevent loading the returned plugin if at least one of the dependencies is missing', () => {
+			expect(() => createApp([
+				asyncPlugin(
+					async () => purePlugin,
+					async () => void 0,
+					[dependentPlugin.id],
+				),
+			])).toThrow(new Error('Application creation failed'));
+
+		});
+		it('should be loaded after the dependencies', () => {
+			let hasPluginB = false;
+
+			createApp([
+				purePlugin,
+				asyncPlugin(
+					async () => dependentPlugin,
+					async (app) => {
+						hasPluginB = app[pluginSymbol].has(purePlugin.id);
+					},
+					[purePlugin.id],
+				),
+			]);
+
+			expect(hasPluginB).toBeTruthy();
+		});
+	});
+	describe('it silently skip plugins already loaded', () => {
+		it('should not throw', () => {
+			expect(() => {
+				createApp([
+					purePlugin,
+					asyncPlugin(
+						async () => purePlugin,
+						async () => void 0,
+					),
+				]);
+			}).not.toThrow();
+		});
+		it('should not load the duplicated plugin', async () => {
+			const app = createApp([
+				purePlugin,
+				asyncPlugin(
+					async () => purePlugin,
+					async () => when.promise,
+				),
+			]);
+
+			app.emitter.on('beforePluginRegistration', spy);
+
+			expect(spy).toHaveBeenCalledTimes(0);
+
+		});
+	});
+	describe('it returns a valid plugins', () => {
+		it('should have an id', () => {
+			const plugin = asyncPlugin(async () => purePlugin, async () => void 0);
+
+			expect(plugin).toHaveProperty('id');
+			expect(plugin.id).toBeTypeOf('symbol');
+		});
+		it('should be a build function', () => {
+			const plugin = asyncPlugin(async () => purePlugin, async () => void 0);
+
+			expect(plugin).toBeTypeOf('function');
+		});
+	});
+	describe('it removes itself when done', () => {
+		it('should remove itself from the app when the plugins are added', async () => {
+			const plugin = asyncPlugin(async () => purePlugin, async () => void 0, [], () => done.resolve());
+			const app = createApp([plugin]);
+
+			await done.promise;
+
+			expect(app[pluginSymbol].has(plugin.id)).toBeFalsy();
+		});
+		// todo should it remove itself in case of failure ?
+	});
+
+	// todo is this really necessary ?
+	describe.skip('it respects the interface', () => {
+		it('should accept an "importer" with a single plugin', async () => {
+			const app = createApp([
+				asyncPlugin(async () => purePlugin, async () => void 0, [], () => done.resolve()),
+			]);
+
+			await done.promise;
+
+			expect(app[pluginSymbol].has(purePlugin.id)).toBeTruthy();
+		});
+		it('should accept an "importer" with an array of plugins', async () => {
+			const app = createApp([
+				asyncPlugin(async () => [purePlugin, dependentPlugin], async () => void 0, [], () => done.resolve()),
+			]);
+
+			await done.promise;
+
+			expect(app[pluginSymbol].has(purePlugin.id)).toBeTruthy();
+			expect(app[pluginSymbol].has(dependentPlugin.id)).toBeTruthy();
+		});
+		it('should accept a list of dependencies', () => {
+
+		});
+		it('should accept not getting a list of dependencies', () => {
+
+		});
+	});
+
+	describe('edge cases', () => {
+		it('should warn when importer returns an empty array', async () => {
+			createApp([asyncPlugin(async () => [], async () => void 0, [], () => done.resolve())]);
+
+			await done.promise;
+
+			expect(warnSpy).toHaveBeenCalledExactlyOnceWith(...warnParams, new Error('asyncPlugin() received no plugin from the importer, did you forget to return them ?'));
+		});
+		it('should not invoke the importer when "when" resolves after the app is destroyed', async () => {
+			const importer = vi.fn(async () => purePlugin);
+			const app = createApp([asyncPlugin(importer, () => when.promise, [], () => done.resolve())]);
+
+			destroyApp(app);
+			when.resolve();
+
+			await done.promise;
+
+			expect(importer).toHaveBeenCalledTimes(0);
+		});
+		it('should not invoke the importer when "when" resolves after the plugin is removed', async () => {
+			const importer = vi.fn(async () => purePlugin);
+			const plugin = asyncPlugin(importer, () => when.promise, [], () => done.resolve());
+
+			const app = createApp([plugin]);
+
+			removePlugin(plugin.id, app);
+			when.resolve();
+
+			await done.promise;
+
+			expect(importer).toHaveBeenCalledTimes(0);
+		});
+		it('should gracefully fail to load plugins with circular dependencies', async () => {
+
+			const app = createApp([
+				asyncPlugin(async () => [circularDep1, circularDep2], () => when.promise, [], () => done.resolve()),
+			]);
+
+			app.emitter.on('failedPluginRegistration', spy);
+
+			when.resolve();
+			await done.promise;
+
+			expect(app[pluginSymbol].has(circularDep1.id)).toBeFalsy();
+			expect(app[pluginSymbol].has(circularDep2.id)).toBeFalsy();
+			expect(warnSpy).toHaveBeenCalledOnce();
+			expect(spy).toHaveBeenCalledExactlyOnceWith({
+				app,
+				reason: new Error(`The plugin "${ String(circularDep1.id) }" declares a dependency that directly or indirectly depends on it.`),
+			});
+		});
+		it('should gracefully fail to load plugins with missing dependencies', async () => {
+			const app = createApp([
+				asyncPlugin(async () => [dependentPlugin], () => when.promise, [], () => done.resolve()),
+			]);
+
+			app.emitter.on('failedPluginRegistration', spy);
+
+			when.resolve();
+			await done.promise;
+
+			expect(app[pluginSymbol].has(dependentPlugin.id)).toBeFalsy();
+			expect(warnSpy).toHaveBeenCalledOnce();
+			expect(spy).toHaveBeenCalledExactlyOnceWith({
+				app,
+				reason: new Error(`The plugin "${ String(dependentPlugin.id) }" depends on "${ String(purePlugin.id) }" but it is not in the list of provided plugins. Did you forget to register it ?`),
+			});
+		});
+		it('should gracefully fail to load non plugins returned by "importer"', async () => {
+			const notAPlugin = vi.fn();
+
+			const app = createApp([
+				asyncPlugin(
+					// @ts-expect-error we are checking that this would lead to a warning
+					async () => [notAPlugin],
+					() => when.promise,
+					[],
+					() => done.resolve(),
+				),
+			]);
+			app.emitter.on('failedPluginRegistration', spy);
+
+			when.resolve();
+			await done.promise;
+
+			expect(warnSpy).toHaveBeenCalledOnce();
+			expect(spy).toHaveBeenCalledExactlyOnceWith({
+				app,
+				reason: new Error(`Array items passed to addPlugins() should all be plugin definitions.`),
+			});
+		});
+	});
+
+	describe('dev mode', () => {
+		it('should warn if importer is synchronous and proceed as usual', async () => {
+			let app = createApp([
+				asyncPlugin(
+					// @ts-expect-error we are checking that a warning is logged in this case
+					() => purePlugin,
+					async () => when.promise,
+					[],
+					() => done.resolve(),
+				),
+			]);
+
+			when.resolve();
+
+			await done.promise;
+
+			// todo factorise this check somewhere
+			expect(warnSpy).toHaveBeenCalledExactlyOnceWith(...warnParams, new Error('asyncPlugin() called with a synchronous importer. This will prevent the plugins added asynchronously from being split into their own chunk when building.'));
+			expect(app[pluginSymbol].has(purePlugin.id)).toBeTruthy();
+		});
+		it('should warn if "when" is synchronous and proceed as usual', async () => {
+			const app = createApp([
+				asyncPlugin(
+					async () => purePlugin, // fakes an async import
+					// @ts-expect-error we are checking that a warning is logged in this case
+					() => void 0,
+					[],
+					() => done.resolve(),
+				),
+			]);
+
+			await done.promise;
+
+			expect(warnSpy).toHaveBeenCalledExactlyOnceWith(...warnParams, new Error('asyncPlugin() called with synchronous condition. If you want to load plugins synchronously use addPlugins() instead.'));
+			expect(app[pluginSymbol].has(purePlugin.id)).toBeTruthy();
+		});
+		it('should warn if importer returns 0 plugin and proceed as usual', async () => {
+			createApp([
+				asyncPlugin(
+					async () => [], // fakes an async import
+					async () => void 0,
+					[],
+					() => done.resolve(),
+				),
+			]);
+
+			await done.promise;
+
+			expect(warnSpy).toHaveBeenCalledExactlyOnceWith(...warnParams, new Error('asyncPlugin() received no plugin from the importer, did you forget to return them ?'));
+		});
+	});
+});
