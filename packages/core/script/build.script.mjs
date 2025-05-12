be5bf695cb9d49acd97914ba4a46dee0356f198c
Ragnar-Oock <u.drgnk@outlook.com>
2025-05-12 19:46:41 +0200

    chore(doc): setup api reference generation

--- packages/core/script/build.script.mjs
+++ packages/core/script/build.script.mjs
@@ -3,21 +3,21 @@ import {spawn} from 'node:child_process';
 
 
 // clear dist folder
-rmSync('./dist', { recursive: true });
+rmSync('./dist', {recursive: true});
 mkdirSync('./dist');
 
 const pkg = JSON.parse(readFileSync('package.json', {encoding: 'utf-8'}));
 
 const targets = [
-    'dev',
-    'prod'
+	'dev',
+	'prod',
 ];
 
 for (const target of targets) {
-    // might lead to issue if we add other targets later...
-    console.log(`building ${pkg.name}, target : ${target}`);
-    await exec('yarn', [`build:${target}`], { stdio: 'inherit' })
-    console.log('-----')
+	// might lead to issue if we add other targets later...
+	console.log(`building ${pkg.name}, target : ${target}`);
+	await exec('yarn', [`build:code:${target}`], {stdio: 'inherit'});
+	console.log('-----');
 }
 
 /**
@@ -28,53 +28,53 @@ for (const target of targets) {
  * @param {object} [options]
  */
 export async function exec(command, args, options) {
-    return new Promise((resolve, reject) => {
-        const _process = spawn(command, args, {
-            stdio: [
-                'ignore', // stdin
-                'pipe', // stdout
-                'pipe', // stderr
-            ],
-            ...options,
-            shell: process.platform === 'win32',
-        })
+	return new Promise((resolve, reject) => {
+		const _process = spawn(command, args, {
+			stdio: [
+				'ignore', // stdin
+				'pipe', // stdout
+				'pipe', // stderr
+			],
+			...options,
+			shell: process.platform === 'win32',
+		});
 
-        /**
-         * @type {Buffer[]}
-         */
-        const stderrChunks = []
-        /**
-         * @type {Buffer[]}
-         */
-        const stdoutChunks = []
+		/**
+		 * @type {Buffer[]}
+		 */
+		const stderrChunks = [];
+		/**
+		 * @type {Buffer[]}
+		 */
+		const stdoutChunks = [];
 
-        _process.stderr?.on('data', chunk => {
-            stderrChunks.push(chunk)
-        })
+		_process.stderr?.on('data', chunk => {
+			stderrChunks.push(chunk);
+		});
 
-        _process.stdout?.on('data', chunk => {
-            stdoutChunks.push(chunk)
-        })
+		_process.stdout?.on('data', chunk => {
+			stdoutChunks.push(chunk);
+		});
 
-        _process.on('error', error => {
-            reject(error)
-        })
+		_process.on('error', error => {
+			reject(error);
+		});
 
-        _process.on('exit', code => {
-            const ok = code === 0
-            const stderr = Buffer.concat(stderrChunks).toString().trim()
-            const stdout = Buffer.concat(stdoutChunks).toString().trim()
+		_process.on('exit', code => {
+			const ok = code === 0;
+			const stderr = Buffer.concat(stderrChunks).toString().trim();
+			const stdout = Buffer.concat(stdoutChunks).toString().trim();
 
-            if (ok) {
-                const result = {ok, code, stderr, stdout}
-                resolve(result)
-            } else {
-                reject(
-                    new Error(
-                        `Failed to execute command: ${command} ${args.join(' ')}: ${stderr}`,
-                    ),
-                )
-            }
-        })
-    })
+			if (ok) {
+				const result = {ok, code, stderr, stdout};
+				resolve(result);
+			} else {
+				reject(
+					new Error(
+						`Failed to execute command: ${command} ${args.join(' ')}: ${stderr}`,
+					),
+				);
+			}
+		});
+	});
 }
