f8a59688d0ac445bda067dbb67dd6b42518d56e2
Ragnar-Oock <u.drgnk@outlook.com>
2025-05-20 16:18:04 +0200

    feat(core): add a defineService call signature for services with interface

--- packages/core/src/services/define-service.ts
+++ packages/core/src/services/define-service.ts
@@ -1,27 +1,51 @@
 import type { Emitter } from 'mitt';
 import type { Application } from '../application';
 import { emitter as createEmitter } from '../emitter';
+import type { Prettify } from '../type-helper';
 import type { Service, ServiceNotifications } from './services.type';
+
+export type NotificationsFromService<service> = service extends Service<infer notification> ? notification : never;
 
 /**
  * Define a stateless service, also known as `Topic`.
  *
+ * {@label TOPIC}
+ *
  * @public
  */
-export function defineService<notification extends Record<string, unknown> = Record<string, unknown>>(): () => Service<notification>;
+export function defineService<notification extends Record<string, unknown> = Record<string, never>>(): () => Service<notification>;
 /**
  * Define a stateful service.
  * @param setup - a service setup function, the object returned by this function will be used as the base for the
  *   derived service instances
  *
+ * {@label STATEFUL_INFERRED}
+ *
  * @public
  */
 export function defineService<
-	notification extends Record<string, unknown> = Record<string, unknown>,
+	notification extends Record<string, unknown> = Record<string, never>,
 	service extends Record<string, unknown> = Record<string, unknown>
 >(
 	setup: (app: Application, emitter: Emitter<notification & ServiceNotifications>) => service,
-): (app: Application) => Service<notification> & service;
+): (app: Application) => Prettify<Service<notification> & service>;
+/**
+ * Define a stateful service.
+ * @param setup - a service setup function, the object returned by this function will be used as the base for the
+ *   derived service instances
+ *
+ * {@label STATEFUL_PRE_TYPED}
+ *
+ * @public
+ */
+export function defineService<
+	service extends Service
+>(
+	setup: (
+		app: Application,
+		emitter: Emitter<NotificationsFromService<service> & ServiceNotifications>,
+	) => Omit<service, keyof Service>,
+): (app: Application) => service;
 /**
  * Use one of the overloads.
  *
@@ -30,15 +54,15 @@ export function defineService<
  * @internal
  */
 export function defineService<
-	notification extends Record<string, unknown> = Record<string, unknown>,
-	service extends Record<string, unknown> = Record<string, unknown>
+	notification extends Record<string, unknown> = Record<string, never>,
+	service extends object = object
 >(
 	setup?: (app: Application, emitter: Emitter<notification & ServiceNotifications>) => service,
 ): (app: Application) => Service<notification> & service {
 	return (app): Service<notification> & service => {
 		const emitter = createEmitter<notification & ServiceNotifications>();
 		return {
-			...(setup ? setup(app, emitter) : {} as service),
+			...(setup?.(app, emitter) ?? {} as service),
 			emitter,
 		};
 	};
