85097091b10118f02490f01f5c93e46fb67db75b
Ragnar-Oock <u.drgnk@outlook.com>
2025-06-06 19:38:43 +0200

    test(panoramique): add basic test for addChild helper

---
+++ plugins/panoramique/tests/browser/helper/add-child.spec.ts
@@ -0,0 +1,81 @@
+import {
+	addPlugins,
+	type Application,
+	createApp,
+	definePlugin,
+	destroyApp,
+	onBeforeDestroy,
+	onCreated,
+	removePlugin,
+} from '@zoram/core';
+import { afterEach, beforeEach, describe, expect, it } from 'vitest';
+import { addChild, panoramiquePlugin } from '../../../src';
+
+describe('addChild', () => {
+
+	let app: Application;
+	const componentId = 'child';
+
+	beforeEach(({ task }) => {
+		app = createApp([ panoramiquePlugin ], { id: task.name });
+	});
+	afterEach(() => {
+		destroyApp(app);
+	});
+
+	describe('life cycle', () => {
+		it('should add child in service on created hook', () => {
+			let hasChildBefore, hasChildAfter;
+
+			addPlugins([
+				definePlugin(() => {
+					onCreated((app) => {
+						hasChildBefore = app.services.panoramique._definitions['root'].children.default.includes(componentId);
+					});
+					addChild('root', componentId);
+					onCreated((app) => {
+						hasChildAfter = app.services.panoramique._definitions['root'].children.default.includes(componentId);
+					});
+				}),
+			], app);
+
+			expect(hasChildBefore).toBeFalsy();
+			expect(hasChildAfter).toBeTruthy();
+		});
+		it('should remove child in service on beforeDestroyed hook', () => {
+			let hasChildBefore, hasChildAfter;
+			const pluginDefinition = definePlugin(() => {
+				onBeforeDestroy((app) => {
+					hasChildBefore = app.services.panoramique._definitions['root'].children.default.includes(componentId);
+				});
+				addChild('root', componentId);
+				onBeforeDestroy((app) => {
+					hasChildAfter = app.services.panoramique._definitions['root'].children.default.includes(componentId);
+				});
+			});
+			addPlugins([
+				pluginDefinition,
+			], app);
+
+			// exec
+			removePlugin(pluginDefinition.id, app);
+
+			expect(hasChildBefore).toBeTruthy();
+			expect(hasChildAfter).toBeFalsy();
+		});
+	});
+	describe('dependencies', () => {
+		it('should mark the host plugin as dependent on panoramique', () => {
+			const pluginDefinition = definePlugin(() => {
+				addChild('root', componentId);
+			});
+
+			const plugin = pluginDefinition();
+
+			expect(plugin.dependencies).toSatisfy(deps =>
+				Array.isArray(deps)
+				&& deps.includes(panoramiquePlugin.id),
+			);
+		});
+	});
+});
