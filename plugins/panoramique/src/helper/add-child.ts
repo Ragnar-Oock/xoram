769a7b15e7a3161364c6237d41d8e714bfc14210
Ragnar-Oock <u.drgnk@outlook.com>
2025-06-11 00:07:41 +0200

    doc(panoramique): add missing TSDoc

--- plugins/panoramique/src/helper/add-child.ts
+++ plugins/panoramique/src/helper/add-child.ts
@@ -3,18 +3,18 @@ import type { Component } from 'vue';
 import type { ComponentSlots } from 'vue-component-type-helpers';
 import { panoramiquePlugin } from '../plugin';
 
-// TODO: usability check
-// TODO: tests ?
 
 /**
  * Register a component as the child of another when the host plugin is created and sever the link on teardown.
  * Additionally, flag the host component as depending on panoramique's plugin.
  *
- * @param parent - id of the parent component to register the child into
- * @param child - id of the child component
- * @param slot - name of the slot to add the child into, defaults to the `default` slot if left empty
+ * @param parent - id of the parent component to register the child into.
+ * @param child - id of the child component.
+ * @param slot - name of the slot to add the child into. Defaults to the `default` slot if left empty.
  * @param index - index, in the given slot, where the child should be inserted. Negative numbers are handled
- *   like `array.at()`.
+ *   like `array.at()`. Defaults to the end of the children array if left empty.
+ *
+ * @public
  */
 export function addChild<parent extends Component = Component>(
 	parent: string,
--- plugins/panoramique/src/plugin.ts
+++ plugins/panoramique/src/plugin.ts
@@ -2,6 +2,9 @@ import { addService, definePlugin } from '@zoram/core';
 import { panoramique } from './service/panoramique.service';
 import { vueService } from './service/vue.service';
 
+/**
+ * Provide the panoramique and vue services to enable the use of Vue components to build dynamic UIs.
+ */
 export const panoramiquePlugin = definePlugin('panoramic', () => {
 	addService('vue', vueService);
 	addService('panoramique', panoramique);
