fa32871eaf04c3b64e2a25be048150e8670b227f
Ragnar-Oock <u.drgnk@outlook.com>
2025-06-12 11:28:39 +0200

    refactor(panoramique): move ServiceCollection augmentation to a dedicated service.ts file

--- plugins/panoramique/src/index.ts
+++ plugins/panoramique/src/index.ts
@@ -1,21 +1,5 @@
-// noinspection ES6UnusedImports
-import type { ServiceCollection } from '@zoram/core'; // eslint-disable-line no-unused-vars
 import type { PanoramiqueService } from './service/panoramique.service';
 import type { VueService } from './service/vue.service';
-
-declare module '@zoram/core' {
-	interface ServiceCollection {
-		/**
-		 * Expose the Vue App instance.
-		 */
-		vue: VueService;
-
-		/**
-		 * Register components and compose them to build your UI.
-		 */
-		panoramique: PanoramiqueService;
-	}
-}
 
 export type {
 	VueService,
---
+++ plugins/panoramique/src/service.ts
@@ -0,0 +1,16 @@
+import type { PanoramiqueService } from './service/panoramique.service';
+import type { VueService } from './service/vue.service';
+
+declare module '@zoram/core' {
+	interface ServiceCollection {
+		/**
+		 * Expose the Vue App instance.
+		 */
+		vue: VueService;
+
+		/**
+		 * Register components and compose them to build your UI.
+		 */
+		panoramique: PanoramiqueService;
+	}
+}
