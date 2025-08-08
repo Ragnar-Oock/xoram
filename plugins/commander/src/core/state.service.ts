782a234fb67daf1a44f8ba2fa798dd2b3d955a8d
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-08-08 20:25:57 +0200

    feat(commander): implement stateService

---
+++ plugins/commander/src/core/state.service.ts
@@ -0,0 +1,22 @@
+import type { Application } from '@xoram/core';
+import { defineService } from '@xoram/core';
+import { failure, type Result, success } from '../api/result';
+import type { Realm, State, StateNotification, StateService } from '../api/state.service';
+import { RealmError } from '../api/state.service';
+
+/**
+ * @internal
+ */
+export const stateService: (app: Application) => StateService = defineService<StateNotification, StateService & State>(
+	() => {
+		return {
+			realms: {},
+			claim(name: string, initialValue: Realm = {}): Result<Readonly<Realm>, RealmError> {
+				if (name in this.realms) {
+					return failure(new RealmError(`Tried to claim realm '${ name }', but it has already been claimed.`));
+				}
+				this.realms[name] = initialValue;
+				return success(this.realms[name]);
+			},
+		};
+	});
