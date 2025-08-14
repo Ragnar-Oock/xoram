b66ab1c68fbc2151252f06d516be83421363bfbe
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-08-14 11:33:36 +0200

    fix(commander): transaction error not passing cause properly

--- plugins/commander/src/api/transaction-error.ts
+++ plugins/commander/src/api/transaction-error.ts
@@ -5,7 +5,7 @@ export class TransactionError extends Error {
 	public transaction: Transaction;
 
 	constructor(msg: string, cause: Error, transaction: Transaction) {
-		super(msg, cause);
+		super(msg, { cause });
 		this.transaction = transaction;
 	}
 }
