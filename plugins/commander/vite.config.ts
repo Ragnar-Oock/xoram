bd1b85380bb78b99feded1d6167008313f386ca9
Ragnar-Oock <ugo.dumontot@gmail.com>
2025-08-24 22:00:55 +0200

    chore(commander): remove vue testing definitions

--- plugins/commander/vite.config.ts
+++ plugins/commander/vite.config.ts
@@ -6,16 +6,7 @@ import pkg from './package.json' with { type: 'json' };
 
 export default defineProject(env =>
 	mergeConfig(
-		mergeConfig(
-			defineLibConfig(pkg)(env),
-			getNodeTestConfig(pkg)(env),
-		),
-		{
-			define: {
-				'__VUE_OPTIONS_API__': false,
-				'__VUE_PROD_DEVTOOLS__': false,
-				'__VUE_PROD_HYDRATION_MISMATCH_DETAILS__': false,
-			},
-		},
+		defineLibConfig(pkg)(env),
+		getNodeTestConfig(pkg)(env),
 	),
 ) as UserProjectConfigFn;
